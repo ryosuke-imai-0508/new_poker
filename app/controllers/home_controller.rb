e3db5a71017624e9c40a445bbc5447e40587d4f5
今井 亮輔 <r.imai@MAC0168.local>
2019-12-10 16:13:37 +0900

    dockerに移行しました

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  include PorkerJudgeService
+
+  def top
+  end
+
+  def judge
+    @cards = params[:hand]
+    @hands = JudgeHands.new(@cards)
+
+    @hands.valid
+    if @hands.error_messages.present?
+      render("home/top")
+    else
+      @hands.execute
+      render("home/top")
+    end
+  end
+
+end
